2d7d5e2daa91544dc6ee12ea19dedd824e2e2d32
Laura <lmcsay@gmail.com>
2021-02-12 15:19:50 -0600

    Feat: Dispatch action creator with stream data

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,4 @@
 // Avoid typos by putting the action types into variables
-export const SIGN_IN = 'SIGN_IN'
-export const SIGN_OUT = 'SIGN_OUT'
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_OUT = 'SIGN_OUT';
+export const CREATE_STREAM = 'CREATE_STREAM';

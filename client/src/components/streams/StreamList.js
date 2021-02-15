7671d0b2744952e8f30932c735d7ca9e8da14f59
Laura <lmcsay@gmail.com>
2021-02-15 12:41:52 -0600

    Feat: Link each stream to appropriate StreamShow component

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -30,7 +30,9 @@ import { fetchStreams } from '../../actions';
                     {this.renderAdminButtons(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
-                        {stream.title}
+                        <Link to={`/streams/${stream.id}`} className="header">
+                            {stream.title}
+                        </Link>
                         <div className="description">{stream.description}</div>
                     </div>
                 </div>
